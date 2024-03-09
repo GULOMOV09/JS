bd1861c110f230640f1a17170631967d9fc6b31b
GULOMOV09 <Alisher>
2024-03-09 11:15:29 +0300

    finished site

---
+++ script.js
@@ -0,0 +1,10 @@
+var yow = +prompt('yowingizni kiriting')
+if (yow<=18){
+  alert('siz oqiwiz kk')
+}else if(yow<=60){
+  alert('siz ishlawingiz kk')
+}else if(yow<=90){
+  alert('siz damoliwiz kk')
+}else if(yow<=100){
+  alert('siz tirik bolsangiz damizi olin vaqt ganimat')
+}
